a35680d5627081bd4ff3fa3f74fdd13bc49cbdd6
unknown <rdn787@bk.ru>
2021-01-21 02:55:51 +0300

    added Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3000;
+        int milePrice = 20;
+        int numberMiles = ticketPrice / milePrice;
+        System.out.println(numberMiles);
+    }
+}
+
